3c8c9f31b31955c6b83bf363700ea89c0a8b1ef4
MiguelGR049 <godoyriveramiguel@gmail.com>
2026-05-13 11:29:19 -0600

    actualizacion api

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://157.230.63.10:3000/api',
+    baseURL: 'http://ximbapp.com:3000/api',
     headers: {
         'Content-Type': 'application/json'
     }
